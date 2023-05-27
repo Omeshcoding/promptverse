31697e7bf48a2407902642485b287100bf8a26b3
umesh <umesharma.us11@gmail.com>
2023-05-27 19:55:44 +0530

    Created header page and configured the app

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import '@styles/globals.css';
+
+import Nav from '@components/Nav';
+
+export const metadata = {
+  title: 'promptVerse',
+  description: 'Discover and share AI Prompts',
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
