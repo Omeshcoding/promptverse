07491ac15a1484f2c416276a8c3b774d5cc0ece5
umesh <umesharma.us11@gmail.com>
2023-05-29 22:46:48 +0530

    Added a form component to the vcreate post page and built the form component

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,29 @@
 'use client';
 import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import Form from '@components/Form';
 
 const CreatePrompt = () => {
-  return <div>WOW</div>;
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    post: '',
+    tag: '',
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
 };
 
 export default CreatePrompt;
